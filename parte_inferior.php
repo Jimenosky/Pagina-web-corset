b6fd12d49fec60dd7b30f8a5155a7da57ee6d91d
Jimena <afjimena25@gmail.com>
2025-04-21 19:21:47 -0600

    Footer

--- parte_inferior.php
+++ parte_inferior.php
@@ -1,4 +1,31 @@
-
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h3>Acerca de Nosotros</h3>
+        <p>En Glamour Ajustado, nuestra pasión por el arte del corset se remonta al empoderamiento. Creemos firmemente que un corset bien confeccionado no es solo una prenda, sino una declaración de estilo y una herramienta para realzar la belleza y la confianza de cada mujer.</p>
+      </div>
+      <div class="col-md-4">
+        <h3>Enlaces Útiles</h3>
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">coleccion</a></li>
+          <li><a href="#">preguntas frecuentes</a></li>
+          <li><a href="#">Ayuda</a></li>
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <h3>Contacto</h3>
+        <p>Dirección: Centro de heredia, 125mts costado parroquia Nuestra Señora de Fatima. </p>
+        <p>Email: GlamourAjustado@yahoo.com</p>
+        <p>Teléfono: +506 1234 5678</p>
+      </div>
+    </div>
+    <div class="copyright">
+      <p>&copy; 2025 Glamour Ajustado. Todos los derechos reservados.</p>
+    </div>
+  </div>
+</footer>
 
 
 
